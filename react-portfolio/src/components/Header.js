d836e8d1847b8d07c27c1a2273ab44f602640c4c
Yusen22 <elijahwilsher@gmail.com>
2023-03-02 10:19:05 +0000

    imported bootstrap. Started header component

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -4,11 +4,29 @@ import "../styles/Header.css";
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
 function Header() {
-  return (
-    <header style={{ backgroundColor: "green" }}>
-      <h1>Welcome</h1>
-    </header>
-  );
+    return (
+        <header className="page-header">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Elijah Wilsher</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
