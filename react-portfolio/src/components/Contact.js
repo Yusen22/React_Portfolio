c392908044ced5575eef48fed77d56a5405679f6
Yusen22 <elijahwilsher@gmail.com>
2023-03-02 10:10:39 +0000

    added project card to components

---
+++ react-portfolio/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Header.css";
+
+// By importing the Header.css file, it is added to the DOM whenever this component loads
+
+function Contact() {
+  return (
+    <header style={{ backgroundColor: "green" }}>
+      <h1>Welcome</h1>
+    </header>
+  );
+}
+
+export default Header;
