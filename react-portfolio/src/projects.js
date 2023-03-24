bd47653e0826bb174a18e46dcc6a2ed30b1088cc
Yusen22 <elijahwilsher@gmail.com>
2023-03-24 02:37:10 +0000

    sorted all images for project cards and styled same cards for uniform sizing

--- react-portfolio/src/projects.js
+++ react-portfolio/src/projects.js
@@ -1,10 +1,26 @@
 const exportedObj = [
   {
     "name": "Password Generator",
-    "description": "Randomly generate a password based on user criteria",
+    "description": "Randomly generate a password based on user's choice of character type",
     "githubRepo": "https://github.com/Yusen22/Password_Generator",
     "deployedApp": "https://yusen22.github.io/Password_Generator/",
-    "screenshot": "./src/images/"
+    "screenshot": require("./images/password-generator-screenshot.png")
+
+  },
+  {
+    "name": "FaveBall Football Hub",
+    "description": "Favourite a team and have access to stats, news and comparisons",
+    "githubRepo": "https://github.com/ionb23/Faveball-app",
+    "deployedApp": "https://faveball.netlify.app/#/",
+    "screenshot": require("./images/faveball-live-page-screenshot.jpg")
+
+  },
+  {
+    "name": "YouTrip Holiday Planner",
+    "description": "Take a personality quiz and get matched with your ideal getaway",
+    "githubRepo": "https://github.com/ionb23/YouTrip",
+    "deployedApp": "https://ionb23.github.io/YouTrip/",
+    "screenshot": require("./images/youtrip-screenshot.png")
 
   },
   {
@@ -12,7 +28,7 @@ const exportedObj = [
     "description": "View a 5-day weather forecast for a chosen city, and save previous searches for future use",
     "githubRepo": "https://github.com/Yusen22/5_Day_Weather_Application",
     "deployedApp": "https://yusen22.github.io/5_Day_Weather_Application/",
-    "screenshot": require("./images/5_day_planner_app_screenshot.png")
+    "screenshot": require("./images/5_day_weather_app_screenshot.png")
 
   },
   {
@@ -20,15 +36,7 @@ const exportedObj = [
     "description": "Use a command-line interface to generate a professional README for a repository",
     "githubRepo": "https://github.com/Yusen22/Command_Line_README_Generator",
     "deployedApp": "N/A",
-    "screenshot": "./images/5_day_planner_app_screenshot.png"
-
-  },
-  {
-    "name": "YouTrip Holiday Planner",
-    "description": "Take a personality quiz and get matched with your ideal getaway",
-    "githubRepo": "https://github.com/Yusen22/Command_Line_README_Generator",
-    "deployedApp": "N/A",
-    "screenshot": "./src/images/5_day_planner_app_screenshot.png"
+    "screenshot": require("./images/README_Generator.png")
 
   },
   {
@@ -36,17 +44,19 @@ const exportedObj = [
     "description": "Take a timed quiz on front-end development, and store previous highscores",
     "githubRepo": "https://github.com/Yusen22/Coding_Quiz",
     "deployedApp": "https://yusen22.github.io/Coding_Quiz/",
-    "screenshot": "./src/images/5_day_planner_app_screenshot.png"
+    "screenshot": "./src/images/5_day_planner_app_screenshot.png",
+    "screenshot": require("./images/Coding_quiz_screenshot.png")
 
   },
   {
     "name": "Daily Planner Application",
-    "description": "Plan out the work day with this colour-coded planner",
+    "description": "Plan out the work day and save your diary with this colour-coded planner",
     "githubRepo": "https://github.com/Yusen22/Planner_Application",
     "deployedApp": "https://yusen22.github.io/Planner_Application/",
-    "screenshot": "./src/images/5_day_planner_app_screenshot.png"
+    "screenshot": require("./images/planner-app-screenshot.gif")
 
   }
+  
 
 ]
 
